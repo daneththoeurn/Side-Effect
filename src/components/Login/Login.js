8438336fcc5d6b4048629d569bb6a78c4ce38fe2
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-23 21:57:48 +0700

    use useReducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,11 +14,22 @@ function emailReducer(state, action) {
   }
   return { value: "", isValid: false };
 }
+
+function passwordReducer(state, action) {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT.BLUR") {
+    //we use state here to guarantee that to be the last state snapshot and make sure it the latest one
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+}
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -26,59 +37,62 @@ const Login = (props) => {
     isValid: null,
   });
 
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
   useEffect(() => {
-    console.log('EFFECT RUNNING');
+    console.log("EFFECT RUNNING");
 
     return () => {
-      console.log('EFFECT CLEANUP');
+      console.log("EFFECT CLEANUP");
     };
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
   //updating React State
   //dependencies are what you using in side effect function and we can omit the state updating function since it insured by React ot never change
-  // useEffect(() => {
-  //    const identifier = setTimeout(() => {
-  //     console.log('checking form validity')
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-
-  //   }, 500);
-  //     //cleanup function
-  //     return () => {
-  //       console.log('CLEANUP!');
-  //       clearTimeout(identifier);
-  //     };
-
-  // }, [enteredEmail, enteredPassword]); //we omit setFormIsValid here
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("checking form validity");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+    //cleanup function
+    return () => {
+      console.log("CLEANUP!");
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]); //we omit setFormIsValid here
 
   const emailChangeHandler = (event) => {
     //if you wanna save what user input you should add some payload such as val
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes("@") && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-    setFormIsValid(
-      emailState.isValid.includes("@") && enteredPassword.trim().length > 6
-    );
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    // setFormIsValid(
+    //   emailState.isValid && event.target.value.trim().length > 6
+    // );
   };
 
-  const validateEmailHandler = () => {
-    //we don't have to add a payload since it to tell us when the input lost focus
-    dispatchEmail({ type: "INPUT_BLUR" });
-  };
+  // const validateEmailHandler = () => {
+  //   //we don't have to add a payload since it to tell us when the input lost focus
+  //   dispatchEmail({ type: "INPUT_BLUR" });
+  // };
 
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+  // const validatePasswordHandler = () => {
+  //   dispatchPassword({ type: 'INPUT_BLUR'});
+  // };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -95,21 +109,21 @@ const Login = (props) => {
             id="email"
             value={emailState.value}
             onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
+            // onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
+            // onBlur={validatePasswordHandler}
           />
         </div>
         <div className={classes.actions}>
