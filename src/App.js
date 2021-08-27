8625f3941d485fa0b4e1ed26f19dafd1985b237b
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-27 22:27:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./components/Context/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  //since we don't have dependencies, the function in useEffect will only run one. Therefore it didn't change compare to the first execution cycle
-  useEffect(() => {
-    const userLoggedInInfo = localStorage.getItem('isLoggedIn');
+  // //since we don't have dependencies, the function in useEffect will only run one. Therefore it didn't change compare to the first execution cycle
+  // useEffect(() => {
+  //   const userLoggedInInfo = localStorage.getItem('isLoggedIn');
 
-    if(userLoggedInInfo === '1'){
-      setIsLoggedIn(true);
-    }
-  
-  }, [])
-  
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+  //   if(userLoggedInInfo === '1'){
+  //     setIsLoggedIn(true);
+  //   }
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+  // }, [])
 
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem('isLoggedIn', '1');
+  //   setIsLoggedIn(true);
+  // };
+
+  // const logoutHandler = () => {
+  //   setIsLoggedIn(false);
+  // };
+
+  const ctx = useContext(AuthContext);
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
